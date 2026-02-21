b1147225e2de334352005503736d0a1e2b7920d2
TunaStark <guralptuna@gmail.com>
2026-02-22 00:02:29 +0300

    hotfix mainpage, removed unused prop

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
       />
       
       <ChatArea
-        recentPrompt=""
         messages={chatLogic.messages}
         prompt={chatLogic.prompt}
         setPrompt={chatLogic.setPrompt}
